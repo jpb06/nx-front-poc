9d9b8cd564f5596b6f9cb5e9fe0f6010e2c9411d
jpb06 <jpb.06@outlook.com>
2021-10-20 23:43:47 +0200

    Mock mutation

--- src/api/useSignupMutation.ts
+++ src/api/useSignupMutation.ts
@@ -1,1 +1,6 @@
-export const useSignupMutation = () => {};
+import { useMutation } from 'react-query';
+
+import { delay } from '@logic/delay';
+
+export const useSignupMutation = () =>
+  useMutation(() => delay(2000).then(() => true));
