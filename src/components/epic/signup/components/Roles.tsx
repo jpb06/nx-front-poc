29339f7c47a9fef58f70a492f0ab4dac2f40ad76
jpb06 <jpb.06@outlook.com>
2021-10-24 22:07:32 +0200

    Parent takes care of displaying a loading block until data is ready

--- src/components/epic/signup/components/Roles.tsx
+++ src/components/epic/signup/components/Roles.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 
 import { useRolesQuery } from '@api/useRolesQuery';
-import { Select, SelectProps } from '@components/generic/forms';
+import { ErrorBlock } from '@components/generic/feedback/error-block/ErrorBlock';
+import { LoadingBlock } from '@components/generic/feedback/loading-block/LoadingBlock';
+import { Select, SelectItem, SelectProps } from '@components/generic/forms';
 
 import { FormModel } from '../types/form-model.type';
 
-type RolesProps = Omit<
-  SelectProps<FormModel>,
-  'name' | 'label' | 'items' | 'isLoading' | 'isError' | 'error' | 'data'
->;
+type RolesProps = Omit<SelectProps<FormModel>, 'name' | 'label' | 'items'>;
+
+const Loading = () => (
+  <LoadingBlock
+    name="loading-roles"
+    text="Loading available roles for you ..."
+  />
+);
 
 export const Roles = (props: RolesProps) => {
-  const query = useRolesQuery();
+  const { status, data } = useRolesQuery();
 
-  return <Select {...props} name="role" label="Role" {...query} />;
+  return (
+    <>
+      {
+        {
+          idle: <Loading />,
+          loading: <Loading />,
+          success: (
+            <Select
+              {...props}
+              name="role"
+              label="Role"
+              data={data as SelectItem[]}
+            />
+          ),
+          error: <ErrorBlock text="An error occured while fetching roles" />,
+        }[status]
+      }
+    </>
+  );
 };
