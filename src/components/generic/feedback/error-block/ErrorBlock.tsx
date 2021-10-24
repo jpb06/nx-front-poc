aabe0b450587bc58578d898c00979c4354e1e237
jpb06 <jpb.06@outlook.com>
2021-10-24 22:06:15 +0200

    Reorganizing generics & adding feedback blocks

---
+++ src/components/generic/feedback/error-block/ErrorBlock.tsx
@@ -0,0 +1,10 @@
+import { Alert } from '@mui/material';
+import React from 'react';
+
+type ErrorBlockProps = {
+  text: string;
+};
+
+export const ErrorBlock: React.FC<ErrorBlockProps> = ({ text }) => (
+  <Alert severity="error">{text}</Alert>
+);
