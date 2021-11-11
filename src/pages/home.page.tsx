1be1d4a3e290655b9208446df4b4f15c0871ab77
jpb06 <jpb.06@outlook.com>
2021-11-11 23:11:54 +0100

    Adding a home page

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+import { Home } from '@components/epic/home/Home';
+
+const HomePage: NextPage = () => <Home />;
+
+export default HomePage;
