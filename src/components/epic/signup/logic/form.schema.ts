dab0866195ca3ddb6ae447061b2f46b768fb33da
jpb06 <jpb.06@outlook.com>
2021-12-11 23:09:38 +0100

    Fixing schema: mui checkbox coerces values into a string

--- src/components/epic/signup/logic/form.schema.ts
+++ src/components/epic/signup/logic/form.schema.ts
@@ -5,7 +5,10 @@ export const schema = z.object({
   lastName: z.string().nonempty('This field is required'),
   password: z.string().nonempty('This field is required'),
   idRole: z.number().gte(0, 'You need to select a role'),
-  idSkills: z.number().array().min(2, 'You need to select at least two skills'),
+  idSkills: z
+    .preprocess((v) => parseInt(z.string().parse(v), 10), z.number())
+    .array()
+    .max(3, 'You need to select at most three skills'),
 });
 
 export type FormModel = z.infer<typeof schema>;
