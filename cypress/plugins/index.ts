d1ceaad1feb1fe9db05d635e6e82857daebaa631
jpb06 <jpb.06@outlook.com>
2021-12-13 20:48:06 +0100

    Excluding cypress folder

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // ***********************************************************
 // This example can be used to load plugins
 //
@@ -12,7 +11,6 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// eslint-disable-next-line no-unused-vars
 const config = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
