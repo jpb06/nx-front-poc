1e8f9f68a970f42707360e834e977317525f0feb
jpb06 <jpb.06@outlook.com>
2021-12-13 20:30:25 +0100

    Fixing cypress tests

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,6 +1,6 @@
-/// <reference types="cypress" />
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// This example can be used to load plugins
 //
 // You can change the location of this file or turn off loading
 // the plugins file with the 'pluginsFile' configuration option.
@@ -12,11 +12,11 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
 // eslint-disable-next-line no-unused-vars
-const config = (on, config) => {
+const config: Cypress.PluginConfig = (
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
