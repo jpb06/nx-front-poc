6fa4e5bc5239a0a3f5de47b5151f1b9d08c03e93
jpb06 <jpb.06@outlook.com>
2021-12-13 20:30:25 +0100

    Fixing cypress tests

--- cypress/integration/signup.spec.ts
+++ cypress/integration/signup.spec.ts
@@ -6,14 +6,11 @@ describe('Signup', () => {
   it('should display validation errors', () => {
     cy.findByRole('button', { name: /signup/i }).click();
 
-    cy.findByText(/firstname is a required field/i);
-    cy.findByText(/lastname is a required field/i);
-    cy.findByText(/password is a required field/i);
-    cy.findByText(/idrole is a required field/i);
-    cy.findByText(/you need to select at least two skills/i);
+    cy.findAllByText(/required/i).should('have.length', 3);
+    cy.findByText(/you need to select a role/i).should('exist');
   });
 
-  it('should display a validation error if less than two skills have been selected', () => {
+  it('should display a validation error if more than three skills have been selected', () => {
     cy.findByLabelText(/lastname/i).type('Mc Bro');
     cy.findByLabelText(/firstName/i).type('Yolo');
     cy.findByLabelText('Password').type('strong-password');
@@ -22,20 +19,23 @@ describe('Signup', () => {
     cy.findByText('Developer').click();
 
     cy.findByText('jest').click();
+    cy.findByText('prisma').click();
+    cy.findByText('nest').click();
+    cy.findByText('react').click();
 
     cy.findByRole('button', { name: /signup/i }).click();
 
-    cy.findByText(/you need to select at least two skills/i);
+    cy.findByText(/you need to select at most three skills/i).should('exist');
   });
 
   it('should display a loading indicator for roles', () => {
-    cy.findByRole('progressbar', { name: /loading-roles/i });
+    cy.findByRole('progressbar', { name: /loading-roles/i }).should('exist');
 
     cy.findByLabelText('Role').should('exist');
   });
 
   it('should display a loading indicator for skills', () => {
-    cy.findByRole('progressbar', { name: /loading-skills/i });
+    cy.findByRole('progressbar', { name: /loading-skills/i }).should('exist');
 
     cy.findByText('Skills').should('exist');
     cy.findAllByRole('checkbox').should('have.length.above', 2);
@@ -55,7 +55,7 @@ describe('Signup', () => {
 
     cy.findByRole('button', { name: /signup/i }).click();
 
-    cy.findByRole('alert');
+    cy.findByRole('alert').should('exist');
     cy.findByText('Invalid role').should('exist');
   });
 
