86bd68b7b6647727f7069288091211a97b18e5d4
jpb06 <jp.bois.06@outlook.fr>
2022-02-27 15:13:42 +0100

    misc path update

--- jest/front/jest.setup.js
+++ jest/front/jest.setup.js
@@ -1,7 +1,7 @@
 require('@testing-library/jest-dom/extend-expect');
 const { matchers } = require('@emotion/jest');
 
-const { server } = require('./../../libs/front/tests/src/msw/server');
+const { server } = require('./../../libs/front/components/src/test/msw/server');
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => server.resetHandlers());
