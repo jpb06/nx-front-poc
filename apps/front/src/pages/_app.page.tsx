5650494593cf719da6d92ffd73e72b5747e749a2
jpb06 <jp.bois.06@outlook.fr>
2022-03-07 15:26:33 +0100

    misc

--- apps/front/src/pages/_app.page.tsx
+++ apps/front/src/pages/_app.page.tsx
@@ -23,7 +23,7 @@ const MyApp: React.FC<EmotionAppProps> = ({
   return (
     <EmotionCacheProvider emotionCache={emotionCache}>
       <Head>
-        <title>My page</title>
+        <title>Sandbox</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <AppThemeProvider>
