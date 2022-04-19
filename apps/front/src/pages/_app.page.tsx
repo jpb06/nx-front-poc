684b023eb334cc1d79d1071d0b9fb940937991d1
jpb06 <jp.bois.06@outlook.fr>
2022-04-19 11:55:17 +0200

    misc

--- apps/front/src/pages/_app.page.tsx
+++ apps/front/src/pages/_app.page.tsx
@@ -20,24 +20,22 @@ const MyApp: React.FC<EmotionAppProps> = ({
   Component,
   emotionCache,
   pageProps,
-}) => {
-  return (
-    <EmotionCacheProvider emotionCache={emotionCache}>
-      <Head>
-        <title>Sandbox</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <AppThemeProvider>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <WithSnackbar>
-          <ReactQueryProvider>
-            <Component {...pageProps} />
-          </ReactQueryProvider>
-        </WithSnackbar>
-      </AppThemeProvider>
-    </EmotionCacheProvider>
-  );
-};
+}) => (
+  <EmotionCacheProvider emotionCache={emotionCache}>
+    <Head>
+      <title>Sandbox</title>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Head>
+    <AppThemeProvider>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <WithSnackbar>
+        <ReactQueryProvider>
+          <Component {...pageProps} />
+        </ReactQueryProvider>
+      </WithSnackbar>
+    </AppThemeProvider>
+  </EmotionCacheProvider>
+);
 
 export default appWithTranslation(MyApp);
