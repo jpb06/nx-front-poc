7717145d756fea4f3cc907078b95077fc19b501c
jpb06 <jp.bois.06@outlook.fr>
2022-04-20 09:50:21 +0200

    bumping agaiiiin

--- libs/front/components/src/test/renders/appRenderHook.tsx
+++ libs/front/components/src/test/renders/appRenderHook.tsx
@@ -1,18 +1,13 @@
-import {
-  renderHook,
-  RenderHookResult,
-  WrapperComponent,
-} from '@testing-library/react-hooks';
-
+import { renderHook, RenderHookResult } from '@testing-library/react';
 import { applyWrappers, ApplyWrappersProps } from './util/applyWrappers';
 
 export const appRenderHook = <TForm, TProps, TResult>(
   callback: (props: TProps) => TResult,
   options?: ApplyWrappersProps<TForm>
-): RenderHookResult<TProps, TResult> => {
+): RenderHookResult<TResult, TProps> => {
   const wrapper = applyWrappers(options);
 
   return renderHook(callback, {
-    wrapper: wrapper as WrapperComponent<TProps>,
+    wrapper,
   });
 };
