6a6089ad5186a22bac76eb08d059cd46562ae957
jpb06 <jp.bois.06@outlook.fr>
2022-04-27 12:04:40 +0200

    fixing jest config files

--- apps/front/jest.config.ts
+++ apps/front/jest.config.ts
@@ -1,3 +1,4 @@
-import getJestOptions from '../../jest/front/jest-options';
+const getJestOptions = require('../../jest/front/jest-options');
 
-module.exports = getJestOptions('apps/front', ['!src/pages/**/*.tsx']);
+const options = getJestOptions('apps/front', ['!src/pages/**/*.tsx']);
+export default options;
--- libs/front/components/jest.config.ts
+++ libs/front/components/jest.config.ts
@@ -1,7 +1,8 @@
 const getJestOptions = require('../../../jest/front/jest-options');
 
-module.exports = getJestOptions('libs/front/components', [
+const options = getJestOptions('libs/front/components', [
   '!src/**/*.styles.ts',
   '!src/test/**/*',
   '!src/providers/**/*',
 ]);
+export default options;
