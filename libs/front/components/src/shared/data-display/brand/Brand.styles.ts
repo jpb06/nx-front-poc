4c62411e22b62ab5ba2db9d81a550b334a5a5164
JP Bois <jp.bois.06@outlook.fr>
2022-08-14 13:30:31 +0200

    chore: bumping ci dependencies (#46)
    * chore: bumping ci dependencies
    
    * chore: fixing brand color issue

--- libs/front/components/src/shared/data-display/brand/Brand.styles.ts
+++ libs/front/components/src/shared/data-display/brand/Brand.styles.ts
@@ -1,10 +1,10 @@
 import { Theme } from '@mui/material';
 import { SxProps } from '@mui/system';
 
-import { AppColor } from '@front/theme';
+import { AppColor, appTheme } from '@front/theme';
 
 export const withThemeColor = (color: AppColor): SxProps<Theme> => {
   return {
-    color,
+    color: appTheme.appColors[color],
   };
 };
