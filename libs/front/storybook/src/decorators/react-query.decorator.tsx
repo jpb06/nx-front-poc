40f7f3d9baa94fc39a34e58b81b4bf2ccc92413d
jpb06 <jp.bois.06@outlook.fr>
2022-11-04 11:25:43 +0100

    chore: suppressing boundaries module error for non buildable libraries

--- libs/front/storybook/src/decorators/react-query.decorator.tsx
+++ libs/front/storybook/src/decorators/react-query.decorator.tsx
@@ -1,5 +1,6 @@
 import { StoryFn } from '@storybook/react/types-6-0';
 
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ReactQueryProvider } from '@front/tests/wrappers';
 
 const ReactQueryWrapper = ReactQueryProvider();
