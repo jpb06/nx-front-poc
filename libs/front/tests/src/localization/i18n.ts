848ec54408f1306526046f76222468a1ed2a862d
jpb06 <jp.bois.06@outlook.fr>
2022-11-04 17:02:47 +0100

    chore: adding middlewares to i18n instance

--- libs/front/tests/src/localization/i18n.ts
+++ libs/front/tests/src/localization/i18n.ts
@@ -1,17 +1,27 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'en',
-  ns: ['common'],
-  defaultNS: 'common',
-  interpolation: {
-    escapeValue: false,
-  },
-  appendNamespaceToMissingKey: true,
-  missingKeyNoValueFallbackToKey: true,
-  resources: { en: {} },
-});
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    ns: ['common', 'cool'],
+    defaultNS: 'common',
+    interpolation: {
+      escapeValue: false,
+    },
+    appendNamespaceToMissingKey: true,
+    missingKeyNoValueFallbackToKey: true,
+    resources: { en: {} },
+
+    react: {
+      useSuspense: false,
+    },
+  });
 
 export { i18n };
