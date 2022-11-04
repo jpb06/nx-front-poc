99d593ce0867f47393511db0329bfc4cc882ee78
jpb06 <jp.bois.06@outlook.fr>
2022-11-04 17:02:59 +0100

    chore: fixing translations in tests

--- libs/front/jest/jest.setup.js
+++ libs/front/jest/jest.setup.js
@@ -2,6 +2,7 @@ require('@testing-library/jest-dom/extend-expect');
 const { matchers } = require('@emotion/jest');
 
 jest.mock('next/dist/client/router', () => require('next-router-mock'));
+jest.mock('next-i18next', () => require('react-i18next'));
 
 const { mswServer } = require('./../../../libs/front/api/src/msw/mswServer');
 
